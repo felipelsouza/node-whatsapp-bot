a1261a517c23197ff81d290c9f09af8f802443e9
Felipe Souza <felipelsouza.dev@gmail.com>
2020-12-18 21:26:22 -0300

    Environment setings

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 module.exports = {
     // API reference URL
-    apiUrl: "",
+    apiUrl: `${process.env.API_URL}`,
     // API token from your personal cabinet
-    token: ""
+    token: `${process.env.API_TOKEN}`
 }
--- index.js
+++ index.js
@@ -7,11 +7,11 @@ app.use(bodyParser.json());
 
 process.on('unhandledRejection', err => {
     console.log(err)
-});	
+});
 
 app.get('/', function (req, res) {
     res.send("It's working.");
-}); 
+});
 
 app.post('/webhook', async function (req, res) {
     const data = req.body;
@@ -20,9 +20,9 @@ app.post('/webhook', async function (req, res) {
         const body = data.messages[i].body;
         const chatId = data.messages[i].chatId;
         const senderName = data.messages[i].senderName;
-        if(data.messages[i].fromMe)return;
-        
-        if(/help/.test(body)){
+        if (data.messages[i].fromMe) return;
+
+        if (/help/.test(body)) {
             const text = `${senderName}, this is a demo bot for https://chat-api.com/.
             Commands:
             1. chatId - view the current chat ID
@@ -30,10 +30,10 @@ app.post('/webhook', async function (req, res) {
             3. ptt - get a voice message
             4. geo - get a location
             5. group - create a group with you and the bot`;
-            await apiChatApi('message', {chatId: chatId, body: text});
-        }else if(/chatId/.test(body)){
-            await apiChatApi('message', {chatId: chatId, body: chatId});
-        }else if(/file (pdf|jpg|doc|mp3)/.test(body)){
+            await apiChatApi('message', { chatId: chatId, body: text });
+        } else if (/chatId/.test(body)) {
+            await apiChatApi('message', { chatId: chatId, body: chatId });
+        } else if (/file (pdf|jpg|doc|mp3)/.test(body)) {
             const fileType = body.match(/file (pdf|jpg|doc|mp3)/)[1];
             const files = {
                 doc: "http://domain.com/tra.docx",
@@ -46,15 +46,15 @@ app.post('/webhook', async function (req, res) {
                 body: files[fileType],
                 filename: `File *.${fileType}`
             };
-            if(fileType == "jpg")dataFile['caption'] = "Text under the photo.";
+            if (fileType == "jpg") dataFile['caption'] = "Text under the photo.";
             await apiChatApi('sendFile', dataFile);
-        }else if(/ptt/.test(body)){            
-            await apiChatApi('sendAudio', {audio: "http://domain.com/tra.ogg", chatId: chatId});
-        }else if(/geo/.test(body)){
-            await apiChatApi('sendLocation', {lat: 51.178843, lng: -1.826210, address: 'Stonehenge', chatId: chatId});
-        }else if(/group/.test(body)){
-            let arrayPhones = [ author.replace("@c.us","") ];
-            await apiChatApi('group', {groupName: 'Bot group', phones: arrayPhones, messageText: 'Welcome to the new group!'});
+        } else if (/ptt/.test(body)) {
+            await apiChatApi('sendAudio', { audio: "http://domain.com/tra.ogg", chatId: chatId });
+        } else if (/geo/.test(body)) {
+            await apiChatApi('sendLocation', { lat: 51.178843, lng: -1.826210, address: 'Stonehenge', chatId: chatId });
+        } else if (/group/.test(body)) {
+            let arrayPhones = [author.replace("@c.us", "")];
+            await apiChatApi('group', { groupName: 'Bot group', phones: arrayPhones, messageText: 'Welcome to the new group!' });
         }
     }
     res.send('Ok');
@@ -64,14 +64,14 @@ app.listen(80, function () {
     console.log('Listening on port 80..');
 });
 
-async function apiChatApi(method, params){
+async function apiChatApi(method, params) {
     const options = {};
     options['method'] = "POST";
     options['body'] = JSON.stringify(params);
     options['headers'] = { 'Content-Type': 'application/json' };
-    
-    const url = `${apiUrl}/${method}?token=${token}`; 
-    
+
+    const url = `${apiUrl}/${method}?token=${token}`;
+
     const apiResponse = await fetch(url, options);
     const jsonResponse = await apiResponse.json();
     return jsonResponse;
